3b65e95cec2b122448387c003ffcdac4895a7b15
allen7u@GitKraken <allen7u@gmail.com>
2022-07-21 23:31:40 +0800

    add client.getIndexes

--- index.js
+++ index.js
@@ -11,10 +11,25 @@
 
 
 
-
-function preQuery() {
+async function preQuery() {
   const client = new MeiliSearch({ host: 'http://127.0.0.1:7700' });
-  client.getStats().then((response)=>{ console.log(response.indexes)})
+  // client.getStats().then((response)=>{ console.log(response.indexes)})
+  client.getIndexes({ limit: 999 }).then((r)=>{
+     console.log(r)
+     console.log(r.results)
+     r.results.forEach( (v,k) => {
+      console.log(k,v)
+      console.log(k,v.uid)
+      // add buttons per index
+      let button = document.createElement('button')
+      button.textContent = v.uid
+      console.log(button)
+      let button_container = document.getElementById('index_chooser')
+      button_container.append(button)
+     })
+     })   
+  // let res = await client.getIndexes({ limit: 3 })
+  // console.log(res)
   console.log('from preQuery')
 }
 
