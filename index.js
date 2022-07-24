4b4925f7185e6f9587cc3ce82f27debbcf8ba8bf
allen7u@GitKraken <allen7u@gmail.com>
2022-07-24 23:30:48 +0800

    add pre-search indexes query - basic

--- index.js
+++ index.js
@@ -4,14 +4,32 @@
 
 
 
+ function getRandomColor() {
+  var letters = 'BCDEF'.split('');
+  var color = '#';
+  for (var i = 0; i < 6; i++ ) {
+      color += letters[Math.floor(Math.random() * letters.length)];
+  }
+  return color;
+}
+
+function randomDivBg(){
+  console.log('from randomDivBg')
+  document.querySelectorAll('div').forEach(function(div){
+    console.log(div)
+    console.log(div.style)
+    console.log(div.textContent)
+    div.style.background = getRandomColor()
+  })
+}
 
 
-
-
-
-
+document.onchange = function(){
+  console.log('from document onchange')
+}
 
 async function preQuery() {
+  
   const client = new MeiliSearch({ host: 'http://127.0.0.1:7700' });
   // client.getStats().then((response)=>{ console.log(response.indexes)})
   client.getIndexes({ limit: 999 }).then((r)=>{
@@ -23,6 +41,8 @@ async function preQuery() {
       // add buttons per index
       let button = document.createElement('button')
       button.textContent = v.uid
+      // button.id = v.uid+'_id'
+      button.onclick = query
       console.log(button)
       let button_container = document.getElementById('index_chooser')
       button_container.append(button)
@@ -33,9 +53,18 @@ async function preQuery() {
   console.log('from preQuery')
 }
 
-function query() {
+function query2(){
+
+  let button_container = document.getElementById('index_chooser')
+      button_container.append('from query2')
+      console.log(this.innerHTML)
+      // console.log(this.getAttribute('id'))
+      button_container.append(this)
+}
+
+function query(index_Name) {
   const search = instantsearch({
-    indexName: "linely",
+    indexName: this.innerHTML,
     searchClient: instantMeiliSearch(
       "http://localhost:7700"
     )
